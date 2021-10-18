41e2805c8d096d2239f315fdb68b7ddd08b387bc
Douglas Almeida <douglas.almeida377@gmail.com>
2021-10-18 13:29:32 -0300

    create quote variention

--- stock-quotes-api/src/main/java/com/simulador/bolsa/stockquotesapi/StockQuotesApiApplication.java
+++ stock-quotes-api/src/main/java/com/simulador/bolsa/stockquotesapi/StockQuotesApiApplication.java
@@ -1,13 +1,33 @@
 package com.simulador.bolsa.stockquotesapi;
 
+import com.simulador.bolsa.stockquotesapi.service.QuoteService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
+
+@Log4j2
+@EnableScheduling
 @SpringBootApplication
 public class StockQuotesApiApplication {
+	private QuoteService quoteService;
+
+	@Autowired
+	public StockQuotesApiApplication(QuoteService quoteService) {
+		this.quoteService = quoteService;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(StockQuotesApiApplication.class, args);
 	}
+
+	@Scheduled(fixedDelay = 1000)
+	public void generateData(){
+		log.info(quoteService.findFirstBySymbolOrderByTimestampDesc("test"));
+	}
+
 
 }
--- stock-quotes-api/src/main/java/com/simulador/bolsa/stockquotesapi/service/QuoteService.java
+++ stock-quotes-api/src/main/java/com/simulador/bolsa/stockquotesapi/service/QuoteService.java
@@ -1,8 +1,13 @@
 package com.simulador.bolsa.stockquotesapi.service;
 
+import com.simulador.bolsa.stockquotesapi.model.Quote;
 import com.simulador.bolsa.stockquotesapi.repository.QuoteRepository;
+import org.apache.commons.math3.random.RandomDataGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.Optional;
 
 @Service
 public class QuoteService {
@@ -12,5 +17,31 @@ public class QuoteService {
     @Autowired
     public QuoteService(QuoteRepository quoteRepository) {
         this.quoteRepository = quoteRepository;
+    }
+
+    public Quote findFirstBySymbolOrderByTimestampDesc(String test) {
+        Optional<Quote> quote = Optional.ofNullable(quoteRepository.findFirstBySymbolOrderByTimestampDesc(test)
+                .map(this::generateNewData)
+                .orElseGet(this::inicializeData));
+
+        return quote.get();
+    }
+
+    private Quote inicializeData() {
+        return quoteRepository.save(Quote.builder()
+                .symbol("test")
+                .openValue(0.222222)
+                .closeValue(0.222222)
+                .timestamp(new Date())
+                .build());
+    }
+
+    private Quote generateNewData(Quote quote) {
+        return quoteRepository.save(Quote.builder()
+                .symbol(quote.getSymbol())
+                .openValue(quote.getOpenValue()* new RandomDataGenerator().nextUniform(-0.10,0.10))
+                .closeValue(quote.getCloseValue()* new RandomDataGenerator().nextUniform(-0.10,0.10))
+                .timestamp(new Date())
+                .build());
     }
 }
