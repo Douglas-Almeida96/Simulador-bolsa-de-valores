1816fa4fbe19ccc0ff3ee3b07f401276a45ef063
Douglas Almeida <douglas.almeida377@gmail.com>
2021-10-18 12:10:47 -0300

    inicializer

---
+++ stock-quotes-api/src/test/java/com/simulador/bolsa/stockquotesapi/StockQuotesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simulador.bolsa.stockquotesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockQuotesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
